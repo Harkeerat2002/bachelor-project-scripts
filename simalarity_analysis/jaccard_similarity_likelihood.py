bea5e9a72d9bdfeeb76cf2dad2ac0e8774961c00
Harkeerat Singh Sawhney <harkeerat@gmail.com>
2024-11-21 13:46:50 +0100

    Changed the Folder Structure

--- simalarity_analysis/jaccard_similarity_likelihood.py
+++
@@ -1,7 +0,0 @@
-def JaccardSimilarity(a, b):
-    a = set(a.split())
-    b = set(b.split())
-    intersection = len(a.intersection(b))
-    union = len(a.union(b))
-    jaccard_similarity = intersection / union
-    return jaccard_similarity
